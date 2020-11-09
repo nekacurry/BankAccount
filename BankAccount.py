6304aa1f2524511d117bea6589324f92a296aa0e
nekacurry <anneka498@gmail.com>
2020-11-09 13:24:13 -0600

    Update BankAccount.py

---
+++ BankAccount.py
@@ -0,0 +1,10 @@
+class BankAccount:
+  def __init__(self, name, aNumber, rNumber, balance):
+  	self.full_name = name
+  	self.account_number = aNumber
+    self.rounting_number = rNumber
+    self.balance = balance
+
+  def deposit(amount):
+    self.amount = amount
+    
